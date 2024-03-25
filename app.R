970f1eba536971d4f2d7d8a36aeb6a7dce1241a6
PalaeoMatNic <PalaeoMatNic>
2024-03-25 15:57:46 +0000

    Initial commit of JNCC app code

---
+++ app.R
@@ -0,0 +1,358 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    http://shiny.rstudio.com/
+#
+
+# 
+# Here are the links to WMS tile layers that we can add
+# 
+# SEPA Marine etc. -- not OK, some issues with naming
+# 
+# https://spatialdata.gov.scot/geonetwork/srv/eng/catalog.search#/metadata/61a675be-a5a7-4e8f-8068-b0cb8f5e8cbb
+# 
+# LCM 2018--OK
+# https://catalogue.ceh.ac.uk/documents/032da3fa-10ba-42cc-b719-b19b6dfd11f5
+
+library(shiny)
+library(bslib)
+library(leaflet)
+library(leaflet.extras)
+library(shinyWidgets)
+library(raster)
+library(readxl)
+library(dplyr)
+library(sp)
+library(sf)
+library(DT)
+library(plotly)
+
+# use renv::install('<pakcage name>') to install more packages
+
+source('theme_elements.R')
+source("data_fun.R")
+
+
+rr <- htmltools::HTML('<a href="https://ceh.ac.uk/" target="_blank"> <img border="0" alt="ImageTitle" src="https://brandroom.ceh.ac.uk/sites/default/files/images/theme/UKCEH-Logo_Long_Pos_RGB_720x170.png" width="auto" height="40"> </a>')
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+    #theme
+    theme = UKCEH_theme,  # << add this line
+    # Application title
+    UKCEH_titlePanel(""),
+    
+    tabsetPanel(
+      tabPanel("Overview",
+        hr(),
+        # Sidebar with a slider input for number of bins 
+        sidebarLayout(
+            sidebarPanel(
+              h4("Chemical Pollution and the Environment"), hr(),
+                # sliderInput("bins",
+                #             "Number of bins:",
+                #             min = 1,
+                #             max = 50,
+                #             value = 30),
+           
+            p("Menu:"),
+            prettySwitch(inputId = "switch1", label = "Map layer view"),
+            prettySwitch(inputId = "switch2", label = "Land use view"),
+            p("Search by location:"),
+            selectInput("country_choice", "Choose country:",
+                        c("England" = "ENG",
+                          "Northern Ireland"="NI",
+                          "Scotland" = "SCOT",
+                          "Wales" = "WAL")),
+            # selectInput("region_choice", "Choose a state:",
+            #             list(`East Coast` = list("NY", "NJ", "CT"),
+            #                  `West Coast` = list("WA", "OR", "CA"),
+            #                  `Midwest` = list("MN", "WI", "IA"))
+            # ),
+            searchInput(
+              inputId = "gridref_choice", 
+              label = "Enter your grid reference :", 
+              placeholder = "SD821672", 
+              btnSearch = icon("search"), 
+              btnReset = icon("remove"), 
+              width = "100%"
+            )
+          ),
+            
+            # Show a plot of the generated distribution
+            mainPanel(
+              leafletOutput("mymap"),
+              p(),
+               # plotOutput("distPlot")
+            )
+        )
+      ),
+      tabPanel("Point data",
+               h3("Pollutant point data"),
+               sidebarLayout(
+                 sidebarPanel(
+                   selectInput("data_choice", "Choose dataset:", 
+                               choices = c("EA water quality GCMS data", "EA pollution inventory 2021")),
+                   uiOutput("dynamic_select"),
+                   # selectInput("gcms_compound", "Choose compound:",
+                   #             c("Phenanthrene", "Benzothiazole", "Cocaine", "Ibuprofen", "2,4,7,9-Tetramethyl-5-decyne-4,7-diol")
+                   #             ),
+                   # selectInput("substance", "Choose pollutant:", 
+                   #             c("Ammonia", "Arsenic", "Mercury", "Particulate matter - total", "Phenols - total as C", "Pentachlorophenol (PCP)", "Perfluoro octanyl sulphate (PFOS)", "Polychlorinated biphenyls (PCBs)" )
+                   #            ),
+                 ),
+                 mainPanel(
+                   verticalLayout(
+                     leafletOutput("pollutant_map"),
+                     plotlyOutput("plot")
+                   ),
+                 )
+               )
+      ),
+      tabPanel("Data table",
+               h3("Selected data"),
+               DTOutput("outputTable"),
+               style = "width: 1200px;"
+        
+      ),
+      tabPanel("Data sources",
+         #includeHTML('data_source.html')
+      ),
+    )
+)  
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+  
+    # import LCM
+    gb_lcm_1km_dom_tar <- raster("./datasets/LCM/gb2021lcm1km_dominant_target.tif")
+    gb_lcm_1km_dom_tar[gb_lcm_1km_dom_tar == 0] <- NA
+    
+    CompoundName <-  "Phenanthrene"
+    NUTS_region <- get_NUTS_regions(NUTS_lvl_code = 1)
+    NUTS_region_with_gcms_data <- data_process_EA_WQ_gcms_with_NUTS(fp_gcms_withNUTS = './datasets/EA_water_quality_GCMS_LCMS/gcms_data_with_NUTS.csv', NUTS_region = NUTS_region, CompoundName = "Phenanthrene")
+    
+    
+    output$mymap <- renderLeaflet({
+      leaflet() %>%
+        addTiles() %>%
+        addPolygons(
+          data = NUTS_region_with_gcms_data,
+          fillColor = ~colorQuantile("viridis", mean_concentration)(mean_concentration),
+          fillOpacity = 0.6,
+          weight = 1,
+          color = "white",
+          popup = ~paste("Compound: ", CompoundName, "<br>",
+                         "Mean Concentration: ", round(mean_concentration, 2), "<br>",
+                         "NUTS ID: ", NUTS_ID),
+          group = "NUTS Level 1"
+        ) %>% 
+        
+        addRasterImage(gb_lcm_1km_dom_tar, opacity = 0.5,
+                       group = "LCM 2021 1km dominant target"
+                       ) %>%
+        addProviderTiles(providers$Stadia.StamenTonerLite,
+                         options = providerTileOptions(noWrap = TRUE) 
+        ) %>% 
+          # addWMSTiles('https://map.sepa.org.uk/server/services/Open/Hydrography/MapServer/WMSServer?request=GetCapabilities&service=WMS',
+          #             layers='12',
+          #             options = WMSTileOptions(crs=27700,opacity=0.5),
+          #             group = 'Scottish coastal areas') %>%  # have some trouble displaying, naming issues.
+          # addWMSLegend('https://map.sepa.org.uk/server/services/Open/Hydrography/MapServer/WMSServer?request=GetLegendGraphic%26version=1.3.0%26format=image/png%26layer=12') %>% 
+          addWMSTiles('https://catalogue.ceh.ac.uk/maps/cca47088-8cdd-4d7a-86b4-90f0a1766364?request=getCapabilities&service=WMS&cache=false&',
+                      layers='HY.PhysicalWaters.Catchments.IHU_AreasWithCoastline',
+                      options = WMSTileOptions(crs=27700,opacity=0.5),
+                      group = 'IHU') %>% 
+          # addWMSTiles('https://catalogue.ceh.ac.uk/maps/032da3fa-10ba-42cc-b719-b19b6dfd11f5?request=getCapabilities&service=WMS&cache=false&',
+          #             layers=c('LC.25m.GB', 'LC.25m.NI'),
+          #             options = WMSTileOptions(crs=27700,opacity=0.5),
+          #             group = 'Land cover map 2018 25m') %>% 
+          leaflet.extras::addWMSLegend('https://catalogue.ceh.ac.uk/maps/032da3fa-10ba-42cc-b719-b19b6dfd11f5?language=eng&version=1.3.0&service=WMS&request=GetLegendGraphic&sld_version=1.1.0&layer=WMS&format=image/png&STYLE=inspire_common:DEFAULT') %>% 
+          setView(-2.7, 54.7, zoom = 4.5) %>%
+          # addLegend(pal = pal, values = ~log10(bird_chem_carcass_data_noLocationNA$`BDE 47`), 
+          #           title = ~'Bird BDE conc ng/g\n wet weight',
+          #           #label = c('3.16','10.0','31.6','100.0','316.2'),
+          #           labFormat = labelFormat(prefix = "10^"),
+          #           opacity = 1,   na.label = "Non detected") %>% 
+          addLayersControl(
+            overlayGroups = c("NUTS Level 1", "LCM 2021 1km dominant target", "IHU","Land cover map 2018 25m"),
+            #overlayGroups = c("base" ),
+            options = layersControlOptions(collapsed = FALSE)
+          ) %>%
+          hideGroup(c("LCM 2021 1km dominant target", "IHU","Land cover map 2018 25m")) %>% 
+          addControl(rr, position = "bottomleft") 
+    })
+    
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
+    
+    
+    # tab 2
+    
+    reactive_df <- reactiveValues(data = NULL)
+    
+    # Function to run when Tab 2 is selected
+    observeEvent(input$tabsetPanel, {
+      if(input$tabsetPanel == "Tab 2") {
+        result <- data_process_EA_pollution()
+        filtered_data <- result[[1]]
+        unique_industry_sector <- result[[2]]
+        
+        # Update the reactive dataframe
+        reactive_df$data <- filtered_data
+        
+        # update input choices
+        updateSelectInput("substance", choices = result[[2]])
+        }
+    })
+    
+    # choice for which data to plot
+    observeEvent(input$data_choice, {
+      if(input$data_choice == "EA pollution inventory 2021") {
+        output$dynamic_select <- renderUI({
+          selectInput("IndustrySector", "Choose Industry Sector:",
+                      #c("Ammonia", "Arsenic", "Mercury", "Particulate matter - total", "Phenols - total as C", "Pentachlorophenol (PCP)", "Perfluoro octanyl sulphate (PFOS)", "Polychlorinated biphenyls (PCBs)")
+                      unique_industry_sector
+          )        
+        })
+      } else if(input$data_choice == "EA water quality GCMS data") {
+        output$dynamic_select <- renderUI({
+          tagList(
+            selectInput("gcms_compound", "Choose compound:",
+                        c("Phenanthrene", "Benzothiazole", "Cocaine", "Ibuprofen", "2,4,7,9-Tetramethyl-5-decyne-4,7-diol")
+                        ),
+            sliderInput("year_slider", "Select Year Range:",
+                        min = min(2013), max = max(2021), 
+                        value = c("2020", "2021")
+            )
+            
+          
+          )
+        })
+      }
+    })
+    
+    
+    observeEvent(input$IndustrySector, {
+      # Generate the filtered data based on the selected substance
+      result <- data_process_EA_pollution(IndustrySector = input$IndustrySector)
+      filtered_data <- result[[1]]
+      
+      # Update the reactive dataframe
+      reactive_df$data <- filtered_data
+      
+      # Generate Leaflet map based on the filtered data
+      output$pollutant_map <- renderLeaflet({
+        leaflet(filtered_data) %>%
+          addTiles() %>%
+          setView(-2.7, 54.7, zoom = 4.5) %>%
+          addControl(html = paste("<h5>", input$IndustrySector, "</h5>", sep = ""), position = "topright") %>%
+          addCircleMarkers(
+            lng = ~Longitude, lat = ~Latitude, radius = ~radius,
+            popup = ~paste(
+              "<b>Substance Name: </b>", substance_name, "<br/>",
+              "<b>Quantity Released: </b>", quantity_released_kg, "kg<br/>",
+              "<b>Sector: </b>", Regulated_Industry_Sector, "<br/>",
+              "<b>Route: </b>", `ROUTE NAME`, "<br/>",
+              sep = ""
+            ),
+            color = "black",   # Outline color
+            fillColor = "blue", # Fill color
+            fillOpacity = 0.5, # Opacity of the fill color
+            weight = 0.5 
+          )
+      })
+    })
+    
+    observeEvent(c(input$year_slider, input$gcms_compound),{
+      filtered_data_gcms <- data_process_EA_WQ_gcms(CompoundName = input$gcms_compound)
+      
+      filtered_data_gcms <- filtered_data_gcms %>% filter(year >= input$year_slider[1], year <= input$year_slider[2])
+      
+      # Update the reactive dataframe
+      reactive_df$data <- filtered_data_gcms
+      
+      # Generate Leaflet map based on the filtered data
+      output$pollutant_map <- renderLeaflet({
+        leaflet(filtered_data_gcms) %>%
+          addTiles() %>%
+          addRasterImage(gb_lcm_1km_dom_tar, opacity = 0.5,
+                         colors = color_data$RGB, 
+                         group = "LCM 2021 1km dominant target") %>% 
+          setView(-2.7, 54.7, zoom = 4.5) %>%
+          addControl(html = paste("<h5>", input$gcms_compound, " ", input$year_slider[1], " - ",  input$year_slider[2],  "</h5>", sep = ""), position = "topright") %>%
+          addCircleMarkers(
+            lng = ~Longitude, lat = ~Latitude, radius = ~radius,
+            popup = ~paste(
+              "<b>Compound Name: </b>", Compound_Name, "<br/>",
+              "<b>Concentration: </b>", Concentration, "<br/>",
+              "<b>Unit: </b>", unit, "<br/>",
+              "<b>Sample description: </b>", SMC_DESC, "<br/>",
+              "<b>Date: </b>", Sample_datetime, "<br/>",
+              sep = ""
+            ),
+            color = "black",   # Outline color
+            fillColor = "blue", # Fill color
+            fillOpacity = 0.5, # Opacity of the fill color
+            weight = 0.5 
+          ) %>% 
+        addLayersControl(
+          overlayGroups = c("LCM 2021 1km dominant target"),
+          #overlayGroups = c("base" ),
+          options = layersControlOptions(collapsed = TRUE)
+        ) %>%
+          hideGroup(c("LCM 2021 1km dominant target"))
+      })
+        # addLegend(pal = paste("'", legend_colors, "'", sep = ""), values = legend_labels,
+        #           position = "bottomleft", title = "Land Cover Legend")
+      
+    })
+    
+    
+    observeEvent(input$map_marker_click, {
+      if(input$data_choice == "EA water quality GCMS data") {
+        event <- input$map_marker_click
+        if (!is.null(event)) {
+          clicked_longitude <- event$lng
+          clicked_latitude <- event$lat
+          
+          selected_data <- filtered_data_gcms[filtered_data_gcms$Longitude == clicked_longitude & filtered_data_gcms$Latitude == clicked_latitude, ]
+          
+          selected_data <- selected_data %>%  filter(n() > 1)
+          
+          # Convert datetime string to POSIXct
+          selected_data$Sample_datetime <- as.POSIXct(selected_data$Sample_datetime, format = "%Y-%m-%d %H:%M:%S")
+          
+          selected_data <- selected_data[order(selected_data$Sample_datetime), ]
+          
+          print(selected_data)
+          
+          output$plot <- renderPlotly({
+            plot_ly(selected_data, x = ~Sample_datetime, y = ~Concentration, type = "scatter", mode = "lines+markers") %>%
+              layout(title = "Time Series Plot", xaxis = list(title = "Datetime"), yaxis = list(title = "Concentration"))
+          })
+        }
+      }
+    })
+    
+    # Render the dataframe in the reactive table
+    output$outputTable  <- renderDT({
+      datatable(reactive_df$data, options = list(scrollX = TRUE))
+    })
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ data_fun.R
@@ -0,0 +1,186 @@
+# function to take EA_pollution_inventory/2021 Pollution Inventory Dataset.xlsx and chosen polluting substance 
+# and return filtered dataframe with lat lon values and normalised data for plotting on map
+data_process_EA_pollution <- function(file_path = './datasets/EA_pollution_inventory/2021 Pollution Inventory Dataset.xlsx', IndustrySector = 'Agriculture', substance = "all") {
+  
+  # Read in the data
+  fp <- file_path 
+  data <- read_excel(fp, skip = 9, col_names = TRUE)
+  
+  # Filter out rows with NA in Easting or Northing columns
+  data <- data[complete.cases(data$EASTING, data$NORTHING), ]
+  
+  # Define the UK National Grid projection
+  uk_proj <- "+init=epsg:27700"  # EPSG code for UK National Grid
+  
+  # Create an sf object using the filtered Easting and Northing coordinates
+  sf_data <- st_as_sf(data, coords = c("EASTING", "NORTHING"), crs = uk_proj)
+  
+  # Transform UK National Grid coordinates to latitude and longitude
+  sf_data <- st_transform(sf_data, crs = 4326)  # EPSG code for WGS84 (latitude and longitude)
+  
+  # Extract latitude and longitude directly from sf_data
+  data$Latitude <- st_coordinates(sf_data)[, 2]
+  data$Longitude <- st_coordinates(sf_data)[, 1]
+  
+  # rename column
+  names(data)[names(data) == "QUANTITY RELEASED (kg)"] <- "quantity_released_kg"
+  names(data)[names(data) == "SUBSTANCE NAME"] <- "substance_name"
+  names(data)[names(data) == "REGULATED INDUSTRY SECTOR"] <- "Regulated_Industry_Sector"
+  
+  
+  
+  # get unique substance and industry names
+  unique_substance_names <- sort(unique(data$substance_name))
+  unique_industry_sector <- sort(unique(data$Regulated_Industry_Sector))
+  
+  # filter for selected  industry section
+  filtered_data <- subset(data, Regulated_Industry_Sector == IndustrySector)
+  
+  filtered_data <- subset(filtered_data, substance_name == substance | substance == "all")
+  
+  
+  
+  
+  # Filter out rows with NA values in the 'quantity_released_kg' column
+  filtered_data <- filtered_data[!is.na(filtered_data$quantity_released_kg), ]
+  
+  # if filtered_data has no rows, i.e. no data in or all NA values in quantity_released_kg column and filtered out in last column
+  if (dim(filtered_data)[1] == 0) {
+    filtered_data[, 'radius'] = NA
+    
+  }else{ # filtered_data does have data in quantity_released_kg column
+    
+    # Calculate the minimum and maximum values
+    min_value <- min(filtered_data$quantity_released_kg)
+    max_value <- max(filtered_data$quantity_released_kg)
+    
+    # Normalize 'quantity_released_kg' to range [0, 1]
+    filtered_data <- filtered_data %>%
+      mutate(quantity_released_kg_norm = (quantity_released_kg - min_value) / (max_value - min_value))
+    
+    ## log bins
+    
+    # Determine the number of bins
+    num_bins <- 5
+    
+    # Create logarithmic bins for normalized quantity values
+    filtered_data <- filtered_data %>%
+      mutate(log_quantity_released_kg_norm = log(quantity_released_kg_norm + 1),  # Adding 1 to avoid taking log of zero
+             bin = cut(log_quantity_released_kg_norm, breaks = seq(min(log_quantity_released_kg_norm), max(log_quantity_released_kg_norm), length.out = num_bins + 1), labels = FALSE))
+    
+    # Define marker sizes for each bin (logarithmic scale)
+    max_size <- 50  # Maximum marker size
+    min_size <- 5   # Minimum marker size
+    bin_sizes <- seq(min(filtered_data$log_quantity_released_kg_norm), max(filtered_data$log_quantity_released_kg_norm), length.out = num_bins + 1)  # Create logarithmic bins
+    bin_sizes <- exp(bin_sizes)  # Convert back to original scale
+    bin_sizes <- pmax(bin_sizes, 1)  # Ensure minimum size is 1
+    bin_sizes <- (bin_sizes - min(bin_sizes)) / (max(bin_sizes) - min(bin_sizes))  # Normalize to [0, 1]
+    bin_sizes <- bin_sizes * (max_size - min_size) + min_size  # Scale to desired range
+    
+    # Assign marker sizes based on the bins
+    filtered_data <- filtered_data %>%
+      mutate(radius = bin_sizes[bin])
+    
+  }
+  
+  return(list(filtered_data, unique_industry_sector, unique_substance_names))
+}
+
+
+# function to import and process EA WQ data
+data_process_EA_WQ_gcms <- function(fp_gcms = './datasets/EA_water_quality_GCMS_LCMS/GCMS Target and Non-Targeted Screening.csv', CompoundName = "Phenanthrene") {
+
+  data_gcms <- read.csv(fp_gcms)
+  
+  filtered_data_gcms <- subset(data_gcms, Compound_Name == CompoundName)
+  
+  # Filter out rows with NA values in the 'quantity_released_kg' column
+  filtered_data_gcms <- filtered_data_gcms[!is.na(filtered_data_gcms$Concentration), ]
+  
+  # Calculate the minimum and maximum values
+  min_value <- min(filtered_data_gcms$Concentration)
+  max_value <- max(filtered_data_gcms$Concentration)
+  
+  # Normalize 'quantity_released_kg' to range [0, 1]
+  filtered_data_gcms <- filtered_data_gcms %>%
+    mutate(Concentration_norm = (Concentration - min_value) / (max_value - min_value))
+  
+  # Determine the number of bins
+  num_bins <- 5
+  
+  # Create logarithmic bins for normalized quantity values
+  filtered_data_gcms <- filtered_data_gcms %>%
+    mutate(log_Concentration_norm = log(Concentration_norm + 1),  # Adding 1 to avoid taking log of zero
+           bin = cut(log_Concentration_norm, breaks = seq(min(log_Concentration_norm), max(log_Concentration_norm), length.out = num_bins + 1), labels = FALSE))
+  
+  # Define marker sizes for each bin (logarithmic scale)
+  max_size <- 30  # Maximum marker size
+  min_size <- 5   # Minimum marker size
+  bin_sizes <- seq(min(filtered_data_gcms$log_Concentration_norm), max(filtered_data_gcms$log_Concentration_norm), length.out = num_bins + 1)  # Create logarithmic bins
+  bin_sizes <- exp(bin_sizes)  # Convert back to original scale
+  bin_sizes <- pmax(bin_sizes, 1)  # Ensure minimum size is 1
+  bin_sizes <- (bin_sizes - min(bin_sizes)) / (max(bin_sizes) - min(bin_sizes))  # Normalize to [0, 1]
+  bin_sizes <- bin_sizes * (max_size - min_size) + min_size  # Scale to desired range
+  
+  # Assign marker sizes based on the bins
+  filtered_data_gcms <- filtered_data_gcms %>%
+    mutate(radius = bin_sizes[bin])
+  
+  return(filtered_data_gcms)
+
+}
+
+
+get_NUTS_regions <- function(NUTS_lvl_code = 1) {
+  library(sf)
+  url = "https://tubcloud.tu-berlin.de/s/RHZJrN8Dnfn26nr/download/NUTS_RG_10M_2021_4326.geojson"
+  spdf <- st_read(url)
+  
+  spdf_UK_NUTS = spdf[(spdf$LEVL_CODE == NUTS_lvl_code & spdf$CNTR_CODE == 'UK'), ] 
+  return(spdf_UK_NUTS)
+}
+
+data_process_EA_WQ_gcms_with_NUTS <- function(fp_gcms_withNUTS = './datasets/EA_water_quality_GCMS_LCMS/gcms_data_with_NUTS.csv', NUTS_region = NUTS_region, CompoundName = "Phenanthrene") {
+  
+  data_gcms_with_NUTS <- read.csv(fp_gcms_withNUTS)
+  
+  # subset data by chosen compound
+  filtered_data <- subset(data_gcms_with_NUTS, Compound_Name == CompoundName)
+  
+  # find mean for each region
+  mean_concentration <- filtered_data %>%
+    group_by(NUTS_ID) %>%
+    summarize(mean_concentration = mean(Concentration, na.rm = TRUE))
+  
+  threshold <- 50  # Set your threshold value here
+  
+  above_threshold <- filtered_data %>%
+    group_by(NUTS_ID) %>%
+    summarize(above_threshold = any(Concentration > threshold, na.rm = TRUE))
+  
+  # merge with nuts df
+  NUTS_region_with_gcms_data <- merge(NUTS_region, mean_concentration, by = "NUTS_ID", all.x = TRUE)
+  
+  return(NUTS_region_with_gcms_data)
+}
+
+
+# Create a data frame for colour-blind friendly LCM colour palette with the RGB values and class names
+color_data <- data.frame(
+  Class = c("Broadleaved woodland", "Coniferous woodland", "Arable", "Improved grassland", "Neutral grassland",
+            "Calcareous grassland", "Acid grassland", "Fen, Marsh and Swamp", "Heather and shrub",
+            "Heather grassland", "Bog", "Inland rock", "Saltwater", "Freshwater", "Supralittoral rock",
+            "Supralittoral sediment", "Littoral rock", "Littoral sediment", "Saltmarsh", "Urban", "Suburban"),
+  RGB = c("#3E3D32", "#004433", "#ECF2BD", "#FEAA73", "#DFE6EC", "#99C9DD", "#E9D6EC", "#76835C",
+          "#806480", "#B29278", "#9BB291", "#B3B3B3", "#000066", "#0000FF", "#B3B300", "#FFFFB3",
+          "#FFFF00", "#FFFF00", "#0080FF", "#000000", "#808080")
+)
+
+# rgb_vector <- as.character(color_data$RGB)
+# class_vector <- as.character(color_data$Class)
+# 
+# # Define legend colors and labels
+# legend_colors <- color_data$RGB
+# legend_labels <- color_data$Class
+
+
